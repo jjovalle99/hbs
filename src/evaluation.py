a146951fb13245c538f1ca3773eeae7eef59a4a1
jjovalle99 <jj.ovalle@uniandes.edu.co>
2024-07-22 08:54:53 -0500

    Checkpoint - open source embeddings working

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,4 +1,6 @@
-# define functions for evaluating retrieval performance
+import pandas as pd
+
+
 def naive_map_at_k(true_ids, predicted_ids, k=10):
     """
     Calculate the Mean Average Precision at K (MAP@K).
@@ -23,5 +25,31 @@ def naive_map_at_k(true_ids, predicted_ids, k=10):
         if p_id in true_ids and p_id not in predicted_ids[:i]:
             num_hits += 1.0
             score += num_hits / (i + 1.0)
+
+    return score / min(len(true_ids), k)
+
+
+def weighted_map_at_k(
+    query_id: int, true_ids: list, predicted_ids: list, label_df: pd.DataFrame, k: int = 10, partial_weight: float = 0.5
+):
+    if not len(true_ids) or not len(predicted_ids):
+        return 0.0
+
+    score = 0.0
+    weighted_hits = 0.0
+
+    for i, product_id in enumerate(predicted_ids[:k]):
+        if product_id in true_ids:
+            label = label_df.loc[(label_df["query_id"] == query_id) & (label_df["product_id"] == product_id), "label"].values[0]
+            if label == "Exact":
+                weight = 1.0
+            elif label == "Partial":
+                weight = partial_weight
+            else:
+                weight = 0.0
+
+            if product_id not in predicted_ids[:i]:
+                weighted_hits += weight
+                score += weighted_hits / (i + 1.0)
 
     return score / min(len(true_ids), k)
